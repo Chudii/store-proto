72158ccab1095fa53de6d9eacbc2057ff2e20f7c
Chudi <chudiibida3@gmail.com>
2022-08-05 19:24:24 -0400

    added files

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
+const port = process.env.PORT || 3003
