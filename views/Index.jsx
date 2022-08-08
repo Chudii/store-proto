007f22218b2ec9260558689c7f8ece6c308876ed
Chudi <chudiibida3@gmail.com>
2022-08-08 01:19:12 -0400

    added footer and added to index.css

--- views/Index.jsx
+++ views/Index.jsx
@@ -17,7 +17,10 @@ class Index extends React.Component {
                 <div>
                     <header>
                         <img src={'/images/medusa-head.png'}></img>
-                        <h1>MEDUSA'S LAIR</h1>
+                        <div className="text">
+                            <h1>MEDUSA'S LAIR</h1>
+                            <h3>The world's first reimagined NFT marketplace.</h3>
+                        </div>
                     </header>
                     <div className='navigation'>
                         <nav>
@@ -68,7 +71,31 @@ class Index extends React.Component {
                             })}
                         </div>
                     </div>
-                    
+                    <div className='footer'>
+                        <div className="col-1">
+                            <h3>USEFUL LINKS</h3>
+                            <a href='#'>About</a>
+                            <a href='#'>Services</a>
+                            <a href='#'>Contact</a>
+                            <a href='#'>Shop</a>
+                            <a href='#'>Blog</a>
+                        </div>
+                        <div className="col-2">
+                            <h3>NEWSLETTER</h3>
+                            <form>
+                                <input type='text' placeholder='Your Email Address' required/>
+                                <br></br>
+                                <button type='submit'>SUBSCRIBE NOW</button>
+                            </form>
+                        </div>
+                        <div className="col-3">
+                            <h3>CONTACT</h3>
+                            <p>Newark, NJ</p>
+                            <p>chudiibida3@gmail.com</p>
+                            <a href='https://linkedin.com/in/chudi-ibida'>My LinkedIn</a>
+                            <a href='https://github.com/Chudii'>My Github</a>
+                        </div>
+                    </div>
                 </div>
             </body>
             </html>
