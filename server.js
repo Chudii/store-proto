1bc4e757ca234d4f5813c5abe2f0ef7812299766
Chudi <chudiibida3@gmail.com>
2022-08-13 10:44:14 -0400

    tinkering expired npm packages

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(express.static('public'))
 app.use(flash())
 app.use(cookieParser())
 app.use(cookieSession())
+app.use(session(app))
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
